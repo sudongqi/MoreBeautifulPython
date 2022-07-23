2f7e17e615804517407c5cd1e80709583bef2b42
Dongqi Su <sudongki@gmail.com>
2022-07-22 20:47:18 -0700

    update

--- examples.py
+++ examples.py
@@ -4,23 +4,46 @@ from aetk import timer, Workers, test_f, min_max_avg, \
 
 
 def run_examples():
-    # use context manager timer() to get execution time
+    # use timer (as a context manager) to get execution time
     with timer():
         d = {i: i for i in range(100000)}
         for i in range(200000):
-            x = d.get(i, None)
+            d.get(i, None)
     '''
     took 0.018950223922729492 seconds
+    '''
+
+    # Workers is more flexible than multiprocessing.Pool
+    with timer():
+        num_workers, num_tasks, fail_rate, exec_time = 4, 8, 0.3, 0.5
+        workers = Workers(f=test_f, num_workers=num_workers)
+        [workers.add_task((i, fail_rate, exec_time)) for _ in range(num_tasks)]
+        [workers.get_res() for _ in range(num_tasks)]
+        workers.terminate()
+    '''
+    worker-0 started
+    worker-1 started
+    worker-2 started
+    worker-3 started
+    worker-0 failed: AssertionError('simulated failure')
+    worker-2 completed
+    worker-0 completed
+    worker-1 completed
+    worker-3 completed
+    worker-3 failed: AssertionError('simulated failure')
+    worker-2 completed
+    worker-0 completed
+    took 1.1288304328918457 seconds
     '''
 
     # dir_of(__file__) return the directory of the current file
     this_dir = dir_of(__file__)
 
-    # path_join == os.path.join
+    # path_join() == os.path.join()
     jsonl_file_path = path_join(this_dir, 'example_data.jsonl')
     json_file_path = path_join(dir_of(__file__), 'example_data.json')
 
-    # print2 is better version of pprint
+    # print2() is a better pprint.pprint()
     print2(load_json(json_file_path), indent=4)
     '''
     {
@@ -44,7 +67,7 @@ def run_examples():
     # load_jsonl() return an iterator of dictionary
     data = list(load_jsonl(jsonl_file_path))
 
-    # print_list() print items in list in separate lines
+    # print_list() print items of a list in separate lines
     print_list(data)
     '''
     {'id': 1, 'name': 'Jackson', 'age': 43}
@@ -52,15 +75,15 @@ def run_examples():
     {'id': 3, 'name': 'Lorelei', 'age': 72}
     '''
 
-    # load_jsonl() can customize loading procedures;
-    # for example, sample 50% from the first 2 items using seed 1234 with no compression
+    # load_jsonl() can customize loading procedures
+    # for example, sample 50% using seed 1234 from the first 2 items
     for d in load_jsonl(jsonl_file_path, take_n=2, sample_ratio=0.5, sample_seed=1234, progress=True, compression=None):
         print(d)
     '''
     {'id': 2, 'name': 'Zunaira', 'age': 24}
     '''
 
-    # print table with dynamic column width
+    # print table with self-adjusted column width
     rows = [d.values() for d in data]
     column_names = list(data[0].keys())
     print_table(rows, column_names, columns_gap_size=3)
@@ -77,7 +100,7 @@ def run_examples():
     ----------text----------
     '''
 
-    # text block that enclose the execution
+    # text block that enclose a snippet
     with text_block(text='table', size=7, char='=', y_gap_size=1):
         print_table(rows, column_names, columns_gap_size=3)
     '''
@@ -91,7 +114,7 @@ def run_examples():
 
     '''
 
-    # get statistics of a int/float list in one for-loop
+    # get 3 key statistics of a list at once
     print(min_max_avg([1, 2, 3, 4, 5]))
     '''
     (1, 5, 3.0)
@@ -102,29 +125,6 @@ def run_examples():
 
     # exec_dir() return the directory you run your python command at
     print(exec_dir())
-
-    # workers class is more flexible than multiprocessing.Pool()
-    with timer():
-        num_workers, num_tasks, fail_rate, exec_time = 4, 8, 0.3, 0.5
-        workers = Workers(f=test_f, num_workers=num_workers)
-        [workers.add_task((i, fail_rate, exec_time)) for _ in range(num_tasks)]
-        [workers.get_res() for _ in range(num_tasks)]
-        workers.terminate()
-    '''
-    worker-0 started
-    worker-1 started
-    worker-2 started
-    worker-3 started
-    worker-0 failed: AssertionError('simulated failure')
-    worker-2 completed
-    worker-0 completed
-    worker-1 completed
-    worker-3 completed
-    worker-3 failed: AssertionError('simulated failure')
-    worker-2 completed
-    worker-0 completed
-    took 1.1288304328918457 seconds
-    '''
 
 
 if __name__ == '__main__':
