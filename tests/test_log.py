885718f23328da7a93fa124754c9c41fae7bb715
Dongqi Su <sudongki@gmail.com>
2023-08-09 02:48:09 -0700

    update

---
+++ tests/test_log.py
@@ -0,0 +1,18 @@
+import sys
+import subprocess
+
+
+def run_process(command):
+    proc = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    exit_code = proc.returncode
+    return exit_code, out, err
+
+
+def test_log():
+    exit_code, out, err = run_process([sys.executable, 'tests/run_log.py'])
+    assert exit_code == 0
+    NL = '\r\n' if sys.platform == 'win32' else '\n'
+    assert out.decode() == f'this is from the global logger{NL}'
+    assert err.decode() == ''
