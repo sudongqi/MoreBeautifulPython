a825695969b7a334be0f035cbe6a45420dc44f84
Dongqi Su <sudongki@gmail.com>
2023-08-09 02:48:22 -0700

    update

--- sync.py
+++ sync.py
@@ -9,7 +9,6 @@ os.system("python -m build")
 os.system("python -m twine upload --repository pypi dist/* --verbose")
 os.system("git commit -a -m \"update\"")
 os.system("git push origin main")
-os.system("git push wakhi main")
 
 for i in range(2):
     os.system("python -m pip install mbp=={}".format(VERSION))
